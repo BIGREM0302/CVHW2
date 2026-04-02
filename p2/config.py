bbda361d16e5a3222bcefd5267b9c69a6efbeb4f
MaxRem <maxzheng0302@gmail.com>
2026-04-02 18:04:15 +0800

    first commit

---
+++ p2/config.py
@@ -0,0 +1,27 @@
+# ============================================================================
+# File: config.py
+# Date: 2026-03-27
+# Author: TA
+# Description: Experiment configurations.
+# ============================================================================
+
+################################################################
+# NOTE:                                                        #
+# You can modify these values to train with different settings #
+# p.s. this file is only for training                          #
+################################################################
+
+# Experiment Settings
+exp_name = 'default'  # name of experiment
+
+# Model Options
+model_type = 'resnet18'  # 'mynet' or 'resnet18'
+#model_type = 'mynet'
+
+# Learning Options
+epochs = 100                # train how many epochs 50->100
+batch_size = 128            # batch size for dataloader 32->128
+use_adam = False           # Adam or SGD optimizer
+lr = 1e-1                  # learning rate 0.01->0.1
+#milestones = [16, 32, 45]  # reduce learning rate at 'milestones' epochs
+milestones = [50, 75, 90]  # reduce learning raet at 'milesontes' epochs
