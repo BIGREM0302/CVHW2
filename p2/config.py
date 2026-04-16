19f1de533cc7d32a2b873945b3303c68e5d9ece4
BIGREM0302 <maxzheng0302@gmail.com>
2026-04-16 04:59:53 +0000

    Acc 0.93 after pretrained weight

--- p2/config.py
+++ p2/config.py
@@ -15,17 +15,19 @@
 exp_name = 'semi'  # name of experiment
 #exp_name = 'mynet_custom_resnet9'
 # Model Options
-model_type = 'resnet18'  # 'mynet' or 'resnet18'
-#model_type = 'mynet'
+#model_type = 'resnet18'  # 'mynet' or 'resnet18'
+model_type = 'mynet'
 
 # Learning Options
 epochs = 100                # train how many epochs 50->100
 batch_size = 128            # batch size for dataloader 32->128
 use_adam = False           # Adam or SGD optimizer
 use_pseudo_labeling = True
-#lr = 1e-1                  # learning rate 0.01->0.1
-lr = 1e-3
+lr = 1e-1                  # learning rate 0.01->0.1
+#lr = 1e-3
+#lr = 1e-2
 #lr = 3e-4 # avoid gradient explode
 #milestones = [16, 32, 45]  # reduce learning rate at 'milestones' epochs
 milestones = [50, 75, 90]  # reduce learning raet at 'milesontes' epochs
+#milestones = [30, 60, 85]
 #milestones = [65, 85]
--- p2/p2_run_train.sh
+++ p2/p2_run_train.sh
@@ -2,5 +2,5 @@
 
 ### Training (We would only run for checking your code is executable)
 ## Should check or modify the dataset path
-python3 p2_train.py --dataset_dir ../hw2_data/p2_data/
-#python3 p2_train.py --dataset_dir /root/hw2_data/p2_data/
+#python3 p2_train.py --dataset_dir ../hw2_data/p2_data/
+python3 p2_train.py --dataset_dir /root/hw2_data/p2_data/
